45edd3ae1fb9a82b40ca5165057fd62af4dfcbca
giancarlo-zulani <slaigox@gmail.com>
2020-11-18 13:56:48 +0100

    new todo check added

--- js/script.js
+++ js/script.js
@@ -6,7 +6,12 @@ const APP = new Vue({
   },
   methods:{
     addTodo : function(){
-      this.todos.push(this.newtodos)
+      if(this.newtodos==='' || this.todos.includes(this.newtodos)){
+        alert('new todo not valid or already inlist')
+      }else{
+        this.todos.push(this.newtodos)
+        this.newtodos = ''
+      };
     },
     removeTodo : function(i){
       this.todos.splice(i,1);
