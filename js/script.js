92750374766856080651025be553e47e00b886d3
gian <slaigox@gmail.com>
2020-11-18 18:23:48 +0100

    animation fixed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const APP = new Vue({
   el:'#app',
   data:{
-    todos : ['fare la cacca','fare schifo','comprare la robbba'],
-    newtodos:''
+    todos : ['fare la cacca','fare schifo','comprare la robbba','rapinare vecchiette','prostituirsi'],
+    newtodos:'',
+    show:true
   },
   methods:{
     addTodo : function(){
